21dac2943c755c125e26276e2c90d2e6c8b97d71
anastasianon <32983332+VekTor2300@users.noreply.github.com>
2022-10-14 22:19:12 +0300

    gitad

---
+++ pr1_calc/src/main/java/com/example/pr1_calc/Pr1CalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.pr1_calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr1CalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr1CalcApplication.class, args);
+	}
+
+}
